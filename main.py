bc703806ea95010fcd8653c415d48d1f607b7504
codeRED-03 <68460284+codeRED-03@users.noreply.github.com>
2023-09-14 17:39:22 +0530

    firstchange

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+os.chdir(r"")
+from ultralytics import YOLO
+import cv2
+
+results = {}
+
+cap = cv2.VideoCapture('')
+frame_no = -1
+ret = True
+while ret:
+    ret, frame = cap.read()
+    if ret:
+        # use YOLO model on every frame
+        results[frame_no] = {}
+        
